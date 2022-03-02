723b1a543417008d25d4570c9b76fac6c6b4d823
Sunghyun Lee <98dltjdgus@naver.com>
2022-03-02 14:57:56 +0900

    "부족한 금액 계산하기" 문제 솔루션2(Java)
    프로그래머스 '코딩테스트 연습 > 위클리 챌린지' 수록 문제에 대한 Java 언어 솔루션 업로드.
    
    초기 풀이 때 구상만 하고 실제로 적용하지 못했던 등차수열 합계 공식을 활용한 문제 솔루션.

---
+++ Practice/Programmers/Weekly-Challenge/부족한 금액 계산하기/CalculateTheInsufficientAmount-ArithmeticalProgressionFormula.java
@@ -0,0 +1,13 @@
+package weekly_challenge;
+
+public class CalculateTheInsufficientAmount {
+
+	public static void main(String[] args) {
+		System.out.println(solution(3, 20, 4));
+	}
+	
+	//부족한 금액 계산하기
+	public static long solution(int price, int money, int count) {
+		return Math.max((count * (2 * price + (count - 1) * price)) / 2 - money, 0);
+	}
+}
