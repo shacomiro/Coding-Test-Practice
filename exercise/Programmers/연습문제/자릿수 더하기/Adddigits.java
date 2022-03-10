052d7d50f57538d2c82478c7da00bf9036fb1391
Sunghyun Lee <98dltjdgus@naver.com>
2022-03-10 16:25:31 +0900

    '자릿수 더하기' 문제 솔루션(Java)
    프로그래머스 '코딩테스트 연습 > 연습문제 > 자릿수 더하기' 문제에 대한 Java 언어 솔루션 업로드.

---
+++ exercise/Programmers/연습문제/자릿수 더하기/Adddigits.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        return Arrays.stream(String.valueOf(n).split("")).mapToInt(x -> Integer.parseInt(x)).sum();
+    }
+}
