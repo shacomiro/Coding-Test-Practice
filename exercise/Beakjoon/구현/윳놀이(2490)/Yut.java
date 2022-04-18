ee2e8d41bfbf41c4e11b4ea588d16b0f8d5a91c6
Sunghyun Lee <98dltjdgus@naver.com>
2022-04-18 12:27:47 +0900

    '윳놀이(2490)' 문제 솔루션(Java)
    백준 온라인 저지 '구현 > 윳놀이' 문제에 대한 Java 언어 솔루션 업로드.

---
+++ exercise/Beakjoon/구현/윳놀이(2490)/Yut.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Yut {
+    public static void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] nums = new int[3];
+
+        for(int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            for(int j = 0; j < 4; j++) {
+                nums[i] += Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < 3; i++) {
+            if(nums[i] == 4)
+                System.out.println("E");
+            else if(nums[i] == 3)
+                System.out.println("A");
+            else if(nums[i] == 2)
+                System.out.println("B");
+            else if(nums[i] == 1)
+                System.out.println("C");
+            else if(nums[i] == 0)
+                System.out.println("D");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Yut.solution();
+    }
+}
