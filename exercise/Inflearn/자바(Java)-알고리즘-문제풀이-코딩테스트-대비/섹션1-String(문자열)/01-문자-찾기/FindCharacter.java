1b775df3e3bddee371b030001bd7530e4e9c7a70
Sunghyun Lee <98dltjdgus@naver.com>
2022-05-06 19:34:07 +0900

    '문자 찾기' 문제 솔루션(Java)
    인프런 '자바(Java) 알고리즘 문제풀이 : 코딩테스트 대비 > 섹션 1. String(문자열) > 문자 찾기' 문제에 대한 Java 언어 솔루션 업로드.

---
+++ exercise/Inflearn/자바(Java)-알고리즘-문제풀이-코딩테스트-대비/섹션1-String(문자열)/01-문자-찾기/FindCharacter.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public int Solution(String str, char target) {
+        int answer = 0;
+        String string = str.toUpperCase();
+        char c = Character.toUpperCase(target);
+
+        for(char charOfstr : string.toCharArray()) {
+            if(charOfstr == c) answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        char c = br.readLine().charAt(0);
+
+        System.out.println(main.Solution(str, c));
+    }
+}
