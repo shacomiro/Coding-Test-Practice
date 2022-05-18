ad5b07696c2f60be6581073d78fb887fa0044499
Sunghyun Lee <98dltjdgus@naver.com>
2022-05-18 14:17:07 +0900

    [Add] '2839(설탕 배달)' 문제 솔루션(Java)
    백준 온라인 저지 '그리디 알고리즘 > 2839(설탕 배달)' 문제에 대한 Java 언어 솔루션 업로드.

---
+++ exercise/Beakjoon/그리디-알고리즘/2839(설탕-배달)/SugarDelivery.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public int solution(int n) {
+        int answer = 0;
+        int cnt3 = 0;
+        int cnt5 = 0;
+
+        while(0 < n) {
+            if(n % 5 == 0) {
+                cnt5++;
+                n -= 5;
+            } else {
+                cnt3++;
+                n -= 3;
+            }
+
+            if(n == 0) answer = cnt3 + cnt5;
+        }
+
+        if(answer == 0) answer = -1;
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main T = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(T.solution(n));
+    }
+}
