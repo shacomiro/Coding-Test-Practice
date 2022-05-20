d7269a5122d1a7a8495bea2c0f6c866f366c6c86
Sunghyun Lee <98dltjdgus@naver.com>
2022-05-21 07:23:11 +0900

    [Create] '3460(이진수)' 문제 솔루션(Java)
    백준 온라인 저지 '수학, 구현 > 3460(이진수)' 문제에 대한 Java 언어 솔루션 업로드.

---
+++ exercise/Beakjoon/구현/3460(이진수)/BinaryNumber.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public String[] solution(int n, int[] arr) {
+        String[] answer = new String[n];
+
+        for(int i = 0; i < n; i++) {
+            int num = arr[i];
+            int cnt = 0;
+            StringBuilder sb = new StringBuilder();
+
+            while(num > 0) {
+                if(num % 2 != 0)  sb.append(cnt).append(" ");
+
+                num /= 2;
+                cnt++;
+            }
+
+            answer[i] = sb.toString();
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
+
+        for(String s : main.solution(n, arr)) System.out.println(s);
+    }
+}
