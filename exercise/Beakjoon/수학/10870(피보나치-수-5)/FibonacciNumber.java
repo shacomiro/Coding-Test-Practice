5ba2b2626edead99c73553eb29e642d65b433c7f
Sunghyun Lee <98dltjdgus@naver.com>
2022-06-01 00:23:56 +0900

    [Create] (백준) '10870(피보나치 수 5)' 문제 솔루션(Java)
    백준 온라인 저지 '수학, 구현 > 10870(피보나치 수 5)' 문제에 대한 Java 언어 솔루션 업로드.

---
+++ exercise/Beakjoon/수학/10870(피보나치-수-5)/FibonacciNumber.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public int solution(int n) {
+        int[] numbers = new int[n + 1];
+
+        return getFibonacci(n, numbers);
+    }
+
+    public int getFibonacci(int n, int[] numbers) {
+        if(numbers[n] != 0) return numbers[n];
+
+        if(n == 0) numbers[n] = 0;
+        else if(n == 1 || n == 2) numbers[n] = 1;
+        else numbers[n] = getFibonacci(n - 2, numbers) + getFibonacci(n - 1, numbers);
+
+        return numbers[n];
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        System.out.println(main.solution(n));
+    }
+}
