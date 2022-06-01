75f6316d1d508b5fc5e4be9799594c3897ec2bd8
Sunghyun Lee <98dltjdgus@naver.com>
2022-06-01 21:02:40 +0900

    [Create] (백준) '2309(일곱 난쟁이)' 문제 솔루션(Java)
    백준 온라인 저지 '브루트포스 알고리즘, 정렬 > 2309(일곱 난쟁이)' 문제에 대한 Java 언어 솔루션 업로드.

---
+++ exercise/Beakjoon/정렬/2309(일곱-난쟁이)/SevenDwarf.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Main {
+    public ArrayList<Integer> solution(ArrayList<Integer> list) {
+        int sum = 0;
+        for(int x : list) sum += x;
+        int target = sum - 100;
+        int dwf1 = 0;
+        int dwf2 = 0;
+
+        findFakeDwarf:
+        for(int i = 0; i < 9; i++)
+            for(int j = 0; j < 9; j++) {
+                if(i == j) continue;
+
+                dwf1 = list.get(i);
+                dwf2 = list.get(j);
+
+                if(dwf1 + dwf2 == target) break findFakeDwarf;
+            }
+
+        list.remove((Integer) dwf1);
+        list.remove((Integer) dwf2);
+        Collections.sort(list);
+
+        return list;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < 9; i++) list.add(Integer.parseInt(br.readLine()));
+        br.close();
+
+        for(int x : main.solution(list)) System.out.println(x);
+    }
+}
