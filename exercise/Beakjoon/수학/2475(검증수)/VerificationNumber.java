f28574ed65b723aa38a92f1ef856881767daa38a
Sunghyun Lee <98dltjdgus@naver.com>
2024-04-25 20:08:12 +0900

    2475번-검증수(브론즈 5, 백준) 문제 오타 수정
    - `OutputStreamReader`를 `OutputStreamriter`로 수정.

--- exercise/Beakjoon/수학/2475(검증수)/VerificationNumber.java
+++ exercise/Beakjoon/수학/2475(검증수)/VerificationNumber.java
@@ -1,14 +1,14 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamReader;
+import java.io.OutputStreamriter;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
 class VerificationNumber {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamReader(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int verifyNum = 0;
         
