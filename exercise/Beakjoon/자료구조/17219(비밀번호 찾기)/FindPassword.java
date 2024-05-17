f52e8d63b2253f281fff63299660f33facd05db8
Sunghyun Lee <98dltjdgus@naver.com>
2024-05-17 21:35:19 +0900

    17219번-비밀번호 찾기(실버 4, 백준)
    - `HashMap`을 사용하여 비밀번호를 `O(1)`만에 찾도록 풀이.

---
+++ exercise/Beakjoon/자료구조/17219(비밀번호 찾기)/FindPassword.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class FindPassword {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st1.nextToken());
+        int m = Integer.parseInt(st1.nextToken());
+
+        HashMap<String, String> credentialsMap = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            credentialsMap.put(st2.nextToken(), st2.nextToken());
+        }
+
+        for(int i = 0; i < m; i++) {
+            bw.write(credentialsMap.get(br.readLine()));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
