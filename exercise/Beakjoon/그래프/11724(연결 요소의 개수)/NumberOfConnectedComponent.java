314d0d5fca42bb3cc13556932634120f25dd3aa4
Sunghyun Lee <98dltjdgus@naver.com>
2024-05-20 18:03:11 +0900

    11724번-연결 요소의 개수(실버 2, 백준)
    - 정점과 간선 정보가 주어졌을 때, 연결 요소(그래프)의 개수를 구하는 문제.
    - 서로 연결된 정점에 대한 정보를 가지는 2차원 `ArrayList`를 활용.
    - 그래프에 있는 각 정점을 차례대로 탐색.
    - 한 번 방문한 정점은 다시 방문하지 않도록 방문 처리.
    - 방문한 정점과 연결된 정점을 아직 방문하지 않은 경우 큐에 추가한 후 방문 처리, 연결된 정점이 없을 때까지 그래프를 계속해서 탐색.
    - 큐가 비면(특정 정점과 이어진 모든 정점 탐색이 끝나면) 특정 연결 요소를 찾은 것. `count`를 증가시켜 연결 요소를 센다.
    - 처음으로 돌아가 다음 정점을 검사한다. 이미 찾은 연결 요소에 속하는 정점은 방문 처리가 되어있으므로, 방문한 경우엔 무시하고 다음 정점으로 넘어간다.

---
+++ exercise/Beakjoon/그래프/11724(연결 요소의 개수)/NumberOfConnectedComponent.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class NumberOfConnectedComponent {
+    public static int solution(ArrayList<ArrayList<Integer>> graph) {
+        int graphSize = graph.size();
+        boolean[] visited = new boolean[graphSize];
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
+
+        for(int i = 1; i < graphSize; i++) {
+            if(visited[i]) {
+                continue;
+            }
+
+            queue.add(i);
+            visited[i] = true;
+
+            while(!queue.isEmpty()) {
+                int current = queue.poll();
+
+                for(int next : graph.get(current)) {
+                    if(visited[next]) {
+                        continue;
+                    }
+
+                    queue.add(next);
+                    visited[next] = true;
+                }
+            }
+
+            queue.clear();
+            count++;
+        }
+        
+        return count;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st1.nextToken());
+        int m = Integer.parseInt(st1.nextToken());
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>(n + 1);
+        for(int i = 1; i <= n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for(int i = 0; i < m; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st2.nextToken());
+            int to = Integer.parseInt(st2.nextToken());
+
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+        }
+
+        bw.write(String.valueOf(solution(graph)));
+        bw.flush();
+        bw.close();
+    }    
+}
