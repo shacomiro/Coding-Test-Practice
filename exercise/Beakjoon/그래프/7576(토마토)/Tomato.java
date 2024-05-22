85b735fac653c11c8b2356c5ab5f4d1625a64a43
Sunghyun Lee <98dltjdgus@naver.com>
2024-05-22 18:36:39 +0900

    7576번-토마토(골드 5, 백준)
    - 2차원 배열을 너비 우선 탐색하여 풀이.
    - 입력을 받을 때, map뿐만 아니라 익지 않은 토마토의 수(`rawTomatosCnt`)와 익은 토마토 목록(`ripenedTomatos`)을 미리 생성. 익은 토마토들은 사전 방문 처리.
    - 익지 않은 토마토가 없다면 즉시 종료.
    - 이외의 경우에는 너비 우선 탐색을 하며 인접한 토마토 익힘 처리. 방문한 토마토가 익지 않은 토마토라면 익힘 처리와 함께 `rawTomatosCnt`를 감소시킨다.
    - 탐색이 끝난 후, 익지 않은 토마토가 남아있는지 여부에 따라 익는 데 걸린 날 또는 -1을 반환한다.

---
+++ exercise/Beakjoon/그래프/7576(토마토)/Tomato.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Tomato {
+    public static int solution(int n, int m, int[][] tomatoBox, boolean[][] visited, LinkedList<int[]> ripenedTomatos, int rawTomatosCnt) {
+        if(rawTomatosCnt == 0) {
+            return 0;
+        }
+        
+        return bfs(ripenedTomatos, rawTomatosCnt, tomatoBox, visited, n, m);
+    }
+
+    public static int bfs(LinkedList<int[]> ripenedTomatos, int rawTomatosCnt, int[][] tomatoBox, boolean[][] visited, int n, int m) {
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        Queue<int[]> queue = ripenedTomatos;
+        int days = -1;
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            
+            for(int i = 0; i < size; i++) {
+                int[] coor = queue.poll();
+                int cx = coor[0];
+                int cy = coor[1];
+
+                if(tomatoBox[cx][cy] == 0) {
+                    tomatoBox[cx][cy] = 1;
+                    rawTomatosCnt--;
+                }
+    
+                for(int j = 0; j < 4; j++) {
+                    int nx = cx + dx[j];
+                    int ny = cy + dy[j];
+    
+                    if(!(0 <= nx && nx < n && 0 <= ny && ny < m)) {
+                        continue;
+                    }
+    
+                    if(tomatoBox[nx][ny] == -1) {
+                        continue;
+                    }
+    
+                    if(visited[nx][ny]) {
+                        continue;
+                    }
+    
+                    queue.add(new int[] {nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+
+            days++;
+        }
+        
+        return rawTomatosCnt > 0 ? -1 : days;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st1.nextToken());
+        int n = Integer.parseInt(st1.nextToken());
+        int[][] tomatoBox = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        LinkedList<int[]> ripenedTomatos = new LinkedList<>();
+        int rawTomatosCnt = 0;
+
+        for(int x = 0; x < n; x++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+
+            for(int y = 0; y < m; y++) {                    
+                int num = Integer.parseInt(st2.nextToken());
+                tomatoBox[x][y] = num;
+
+                if(num == 0) {
+                    rawTomatosCnt++;
+                }
+
+                if(num == 1) {
+                    ripenedTomatos.add(new int[] {x, y});
+                    visited[x][y] = true;
+                }
+            }
+        }
+        
+        bw.write(String.valueOf(solution(n, m, tomatoBox, visited, ripenedTomatos, rawTomatosCnt)));
+        bw.flush();
+        bw.close();
+    }
+}
