ae492d7110f3ff0716a428b6c083fa493b796727
Sunghyun Lee <98dltjdgus@naver.com>
2024-07-15 15:55:42 +0900

    3273번-두 수의 합(실버 3, 백준)
    - 정렬과 투 포인터를 활용한 문제 풀이.
    - 서로 다른 두 수의 합이 특정 값을 만족하는 쌍을 구해야 하므로,  중복을 고려하지 않아도 된다.
    - 주어진 수열을 미리 정렬한 후 양 끝에서 시작하여 두 수의 합을 구한다.
    - 합계보다 클 경우 오른쪽 포인터를 내리고, 합계보다 작다면 왼쪽 포인터를 올린다. 이를 반복하면서 모든 조합의 합계를 비교한다.
    - 목표값과 일치할 경우 카운트를 올리며, 포인터가 만나면 반복을 종료한다.

---
+++ exercise/Beakjoon/정렬/3273(두 수의 합)/Problem3273.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Solution3273 {
+    private int[] numbers;
+    private int target;
+
+    public Solution3273(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+    }
+
+    public String solve() {
+        int count = 0;
+        int lt = 0;
+        int rt = numbers.length - 1;
+
+        Arrays.sort(numbers);
+
+        while(lt < rt) {
+            int sum = numbers[lt] + numbers[rt];
+
+            if(sum == target) {
+                count++;
+            }
+
+            if(sum > target) {
+                rt--;
+            } else {
+                lt++;
+            }
+        }
+
+        return String.valueOf(count);
+    }
+}
+
+public class Problem3273 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int x = Integer.parseInt(br.readLine());
+
+        bw.write(new Solution3273(arr, x).solve());
+        bw.flush();
+        bw.close();
+    }
+}
