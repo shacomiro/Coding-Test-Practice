1080415138c24c886d0aba6a21e5d8dcf6cef2cc
Sunghyun Lee <98dltjdgus@naver.com>
2024-07-25 15:34:23 +0900

    16954번-움직이는 미로 탈출(골드 3, 백준)
    - 너비 우선 탐색을 활용한 문제 풀이.
    - 캐릭터는 사방+대각선+제자리의 총 9가지 이동이 가능하다.
    - 제자리에 머무르는 것이 가능하므로, 현재 자리에 머무르기 위해서는 큐에 들어있는 요소를 꺼낼 때마다 방문 처리를 초기화해야 한다.
    - 특히, 처음 시작 시 제자리에 머무를 수 있도록 방문 여부를 false로 시작해야 한다.
    - 벽 정보가 누락되는 것을 막기 위해 맨 아랫줄 벽부터 이동한다.
    - 현재 캐릭터의 위치로 벽이 이동한 경우 해당 위치는 더 이상 탐색하지 않는다.

---
+++ exercise/Beakjoon/그래프/너비 우선 탐색/16954(움직이는 미로 탈출)/Problem16952.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+class Solution16952 {
+    private final int[] DX = {0, 1, 0, 1, 1, -1, -1, 0, -1};
+    private final int[] DY = {0, 0, 1, 1, -1, 1, 0, -1, -1};
+    private char[][] map;
+    private boolean[][] visited;
+    
+    public Solution16952(char[][] map) {
+        this.map = map;
+        this.visited = new boolean[8][8];
+    }
+
+    public String solve() {
+        return String.valueOf(bfs());
+    }
+
+    private int bfs() {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[] {7, 0});
+
+        while(!queue.isEmpty()) {
+            int qSize = queue.size();
+
+            for(int i = 0; i < qSize; i++) {
+                int[] curr = queue.poll();
+                int cx = curr[0];
+                int cy = curr[1];
+                
+                if(map[cx][cy] == '#') {
+                    continue;
+                }
+                if(cx == 0 && cy == 7) {
+                    return 1;
+                }
+    
+                for(int j = 0 ; j < 9; j++) {
+                    int nx = cx + DX[j];
+                    int ny = cy + DY[j];
+    
+                    if(!(0<= nx && nx < 8 && 0 <= ny && ny < 8)) {
+                        continue;
+                    }
+                    if(visited[nx][ny]) {
+                        continue;
+                    }
+                    if(map[nx][ny] == '#') {
+                        continue;
+                    }
+    
+                    queue.offer(new int[] {nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+
+            visited = new boolean[8][8];
+            moveWalls();
+        }
+
+        return 0;
+    }
+
+    private void moveWalls() {
+        for(int i = 6; i >= 0; i--) {
+            for(int j = 0; j < 8; j++) {
+                map[i + 1][j] = map[i][j];
+            }
+        }
+        Arrays.fill(map[0], '.');
+    }
+}
+
+public class Problem16952 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        char[][] arr = new char[8][8];
+        for(int i = 0; i < 8; i++) {
+            char[] chars = br.readLine().toCharArray();
+            for(int j = 0; j < 8; j++) {
+                char c = chars[j];
+                arr[i][j] = c;
+            }
+        }
+        
+        bw.write(new Solution16952(arr).solve());
+        bw.flush();
+        bw.close();
+    }
+}
