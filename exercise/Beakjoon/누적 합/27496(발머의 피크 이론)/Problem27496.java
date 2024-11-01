ee22d945dadaf545749db0ba07ebd08b656dd53d
Sunghyun Lee <98dltjdgus@naver.com>
2024-11-01 23:47:32 +0900

    27496번-발머의 피크 이론(실버 3, 백준)
    - 누적 합, 슬라이딩 윈도우를 활용한 문제 풀이.
    - 특정 기간 동안의 알코올 농도가 정해진 범위 내를 유지하는 지 확인해야 한다.
    - 매 시간 마시는 알코올의 누적 합에서 정해진 범위를 벗어나는 것을 제외한 값을 계산한다.
    - 해당 값이 농도 범위 내에 유지하는 횟수를 센다.
    - 모든 시간에 대한 검사가 끝난 후 최종적으로 구한 유지 시간을 출력한다.

---
+++ exercise/Beakjoon/누적 합/27496(발머의 피크 이론)/Problem27496.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Solution27496 {
+    private int endTime;
+    private int limit;
+    private int[] alchohol;
+
+    public Solution27496(int endTime, int limit, int[] alchohol) {
+        this.endTime = endTime;
+        this.limit = limit;
+        this.alchohol = alchohol;
+    }
+
+    public String solve() {
+        int currTime = 0;
+        int currAlchohol = 0;
+        int pass = 0;
+
+        while(currTime < endTime) {
+            currAlchohol += alchohol[currTime] - ((0 <= currTime - limit) ? alchohol[currTime - limit] : 0);
+
+            if(129 <= currAlchohol && currAlchohol <= 138) {
+                pass++;
+            }
+
+            currTime++;
+        }
+
+        return String.valueOf(pass);
+    }
+}
+
+public class Problem27496 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        bw.write(new Solution27496(n, l, arr).solve());
+        bw.flush();
+        bw.close();
+    }
+}
