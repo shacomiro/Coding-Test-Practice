79370e1e9098eeff14178210693e74e6b33047e5
Sunghyun Lee <98dltjdgus@naver.com>
2024-11-09 19:17:05 +0900

    1005번-ACM Craft(골드 3, 백준)
    - 다이나믹 프로그래밍, 그래프 이론, 위상 정렬, 방향 비순환 그래프를 활용한 문제 풀이.
    - 선행 작업이 완료된 시점 기준으로 작업을 시작해야 하므로, 위상 정렬을 사용해야 한다.
    - 단순히 순서 뿐만 아니라, 현재 누적된 작업 시간을 고려해서 최단 시간을 구해야 하기 떄문에 다이나믹 프로그래밍도 사용해야 한다.
    - 먼저 각각의 건물들의 선행 조건에 맞춰 탐색하는 위상 정렬 로직을 구현한다.
    - 각 건물을 건설하는 데 걸리는 최대 시간을 갖는 별도의 DP 배열을 생성한다.
    - 로직 안에서 선행 건물에서 후속 건물을 방문할 때, 후속 건물의 현재 최대 시간과 선행 건물의 최대 시간+후속 건물 건설 시간을 비교해서 더 큰 값을 후속 건물의 최대 시간으로 갱신한다.
    - 위 과정을 포함한 위상 정렬을 모두 마친 후, DP 배열에서 목표 건물의 최대 시간을 구해 반환한다.

---
+++ exercise/Beakjoon/그래프/정렬/위상 정렬/1005(ACM Craft)/Problem1005.java
@@ -0,0 +1,98 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Solution1005 {
+    private int size;
+    private int[] delay;
+    private List<List<Integer>> graph;
+    private int[] inDegree;
+    private int target;
+
+    public Solution1005(int size, int[] delay, List<List<Integer>> graph, int[] inDegree, int target) {
+        this.size = size;
+        this.delay = delay;
+        this.graph = graph;
+        this.inDegree = inDegree;
+        this.target = target;
+    }
+
+    public String solve() {
+        return String.valueOf(topologicalSort(target));
+    }
+
+    private int topologicalSort(int target) {
+        int[] maxDelay = new int[size + 1];
+        Queue<Integer> queue = new ArrayDeque<>();
+        
+        for(int i = 1; i <= size; i++) {
+            maxDelay[i] = delay[i];
+            if(inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while(!queue.isEmpty()) {
+            for(int i = 0; i < size; i++) {
+                int curr = queue.poll();
+
+                for(int next : graph.get(curr)) {
+                    maxDelay[next] = Math.max(maxDelay[next], maxDelay[curr] + delay[next]);
+                    inDegree[next]--;
+
+                    if(inDegree[next] == 0) {
+                        queue.offer(next);
+                    }
+                }
+            }
+        }
+
+        return maxDelay[target];
+    }
+}
+
+public class Problem1005 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int[] arr1 = new int[n + 1];
+            st = new StringTokenizer(br.readLine());
+            for(int j = 1; j <= n; j++) {
+                arr1[j] = Integer.parseInt(st.nextToken());
+            }
+            int[] arr2 = new int[n + 1];
+            List<List<Integer>> list = new ArrayList<>();
+            for(int j = 0; j <= n; j++) {
+                list.add(new ArrayList<>());
+            }
+            for(int j = 0; j < k; j++) {
+                st = new StringTokenizer(br.readLine());
+                int first = Integer.parseInt(st.nextToken());
+                int next = Integer.parseInt(st.nextToken());
+
+                list.get(first).add(next);
+                arr2[next]++;
+            }
+            int w = Integer.parseInt(br.readLine());
+
+            bw.write(new Solution1005(n, arr1, list, arr2, w).solve());
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
