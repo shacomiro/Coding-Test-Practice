def7e127b57ce91174b5510c214a6119d3249278
Sunghyun Lee <98dltjdgus@naver.com>
2024-12-05 17:55:28 +0900

    10709번-기상캐스터(실버 5, 백준)
    - 구현, 시뮬레이션을 활용한 문제 풀이.
    - h x w 크기의 하늘에서 구름이 동쪽 방향으로 진행할 때, 각 구역에 구름이 도착할 때까지 걸리는 예상 시간을 구해야 한다.
    - 진행 방향이 동쪽이므로, 하늘의 각 행에 대해서 검사를 수행한다.
    - 구름이 도착하지 않는 값 -1을 초기 구름 도달 시간으로 설정한다.
    - 동쪽으로 이동하면서 현재 위치에 구름이 있는지 확인하고, 구름을 만나면 도달 예정 시간을 0으로 갱신한다.
    - 구름이 없을 경우, 구름을 이미 만난 경우(cloudTime > -1) 이전 도달 시간 값에 1을 더하고, 만나지 않은 경우 -1을 그대로 사용한다.
    - 각 위치에 해당하는 도달 예정 시간을 기록한다.
    - 하늘의 모든 구역에 대한 검사를 끝낸 후, 기록된 시간을 출력한다.

---
+++ exercise/Beakjoon/시뮬레이션/10709(기상캐스터)/Problem10709.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Solution10709 {
+    private int h;
+    private int w;
+    private char[][] sky;
+    private int[][] times;
+
+    public Solution10709(int h, int w, char[][] sky) {
+        this.h = h;
+        this.w = w;
+        this.sky = sky;
+        this.times = new int[h][w];
+    }
+
+    public String solve() {
+        StringBuilder sb = new StringBuilder();
+
+        for(int x = 0; x < h; x++) {
+            int cloudTime = -1;
+
+            for(int y = 0; y < w; y++) {
+                cloudTime = (sky[x][y] == 'c') ? 0 : 
+                            (cloudTime >= 0) ? cloudTime + 1 : -1;
+                
+                sb.append(cloudTime).append(" ");
+            }
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+}
+
+public class Problem10709 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        char[][] arr = new char[h][w];
+        for(int i = 0; i < h; i++) {
+            char[] chars = br.readLine().toCharArray();
+            for(int j = 0; j < w; j++) {
+                arr[i][j] = chars[j];
+            }
+        }
+
+        bw.write(new Solution10709(h, w, arr).solve());
+        bw.flush();
+        bw.close();
+    }
+}
