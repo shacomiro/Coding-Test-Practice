d938298399fe4a0127fb9bb98bde55f6a418ce39
Sunghyun Lee <98dltjdgus@naver.com>
2025-03-14 15:06:06 +0900

    9625번-BABBA(실버 5, 백준)
    - 다이나믹 프로그래밍을 활용한 문제 풀이.
    - 버튼을 K번 눌렀을 때 화면에 표시되는 A, B의 개수를 구해야 한다.
    - A -> B -> AB -> BAB -> BABBA 순으로 변한다. A는 B로 바뀌고, B는 B는 그대로 둔 채 A가 추가되는 방식이다.
    - 직전 B에 A가 이어붙는 방식이기 때문에 i번째 A는 i-1번째 B의 수와 같다. 반대로 B는 직전 B의 수에서 직전 A가 B로 바뀐 것을 더한 것과 같다.
    - 위 규칙에 따라 버튼을 누르기 전인 i=0 상태를 미리 지정하고, i를 1부터 K까지 순회하면서 i번째 A, B의 개수를 구한다.
    - 마지막에 구한 K번 버튼을 누른 후 표시되는 A, B의 개수를 출력한다.

---
+++ exercise/Beakjoon/다이나믹 프로그래밍/9625(BABBA)/Problem9625.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Solution9625 {
+    private int k;
+    private int[][] dp;
+
+    public Solution9625(int k) {
+        this.k = k;
+        this.dp = new int[k + 1][2];
+    }
+
+    public String solve() {
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+
+        for(int i = 1; i <= k; i++) {
+            dp[i][0] = dp[i - 1][1];
+            dp[i][1] = dp[i - 1][0] + dp[i - 1][1];
+        }
+
+        return String.valueOf(dp[k][0] + " " + dp[k][1]);
+    }
+}
+
+//A->B
+//B->BA
+
+//A         1 0
+//B         0 1
+//BA        1 1
+//BAB       1 2
+//BABBA     2 3
+//BABBABAB  3 5
+
+public class Problem9625 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(new Solution9625(Integer.parseInt(br.readLine())).solve());
+        bw.flush();
+        bw.close();
+    }
+}
