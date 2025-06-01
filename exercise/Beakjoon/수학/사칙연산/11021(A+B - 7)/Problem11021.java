6a45df8c7291c54278eee95be450f94899af7fb7
Sunghyun Lee <98dltjdgus@naver.com>
2025-06-01 20:35:04 +0900

    11021번-A+B - 7(브론즈 5, 백준)
    - 수학, 구현, 사칙연산을 활용한 문제 풀이.
    - 두 수 A, B를 더한 결과를 주어진 형식에 맞춰 출력한다.

---
+++ exercise/Beakjoon/수학/사칙연산/11021(A+B - 7)/Problem11021.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+class Solution11021 {
+    private int id;
+    private int a;
+    private int b;
+
+    public Solution11021(int id, int a, int b) {
+        this.id = id;
+        this.a = a;
+        this.b = b;
+    }
+
+    public String solve() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Case #").append(id).append(": ").append(a + b);
+
+        return sb.toString();
+    }
+}
+
+public class Problem11021 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= t; i++) {
+            st = new StringTokenizer(br.readLine());
+            bw.write(new Solution11021(i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())).solve());
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
