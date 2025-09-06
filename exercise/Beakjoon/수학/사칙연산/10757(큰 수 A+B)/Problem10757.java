561297d02789fce1739dd35762ad07a05f48e6dc
Sunghyun Lee <98dltjdgus@naver.com>
2025-09-06 11:07:27 +0900

    10757번-클래스명 수정
    - 파일 이름과 일치하지 않는 클래스명을 문제 번호에 맞도록 수정.

--- exercise/Beakjoon/수학/사칙연산/10757(큰 수 A+B)/Problem10757.java
+++ exercise/Beakjoon/수학/사칙연산/10757(큰 수 A+B)/Problem10757.java
@@ -6,11 +6,11 @@ import java.io.OutputStreamWriter;
 import java.math.BigInteger;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Problem10757 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         BigInteger a = new BigInteger(st.nextToken());
         BigInteger b = new BigInteger(st.nextToken());
