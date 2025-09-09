6548b4b398b45dc8edaf2f401bc6d956a6548d6b
Sunghyun Lee <98dltjdgus@naver.com>
2025-09-09 20:09:05 +0900

    2965번-캥거루 세마리(브론즈 3, 백준)
    - 수학을 활용한 문제 풀이.
    - 바깥쪽 캥거루가 다른 두 캥거루 사이로 점프할 때 최대로 움직이는 횟수를 구해야 한다.
    - 캥거루들의 초기 위치 중 각 캥거루 사이의 간격이 가장 넓은 곳으로 점프를 시작하면 가장 오랫동안 점프할 수 있다.
    - 점프 횟수는 내부 간격보다 1 작으므로 간격에서 1을 뺀 값을 출력한다.

---
+++ exercise/Beakjoon/수학/2965(캥거루 세마리)/Problem2965.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Problem2965 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        bw.write(new Solver(a, b, c).solve());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        private int a;
+        private int b;
+        private int c;
+
+        public Solver(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public String solve() {
+            return String.valueOf(Math.max(b - a, c - b) - 1);
+        }
+    }
+}
