5e22d916c327f78b64452e82808846e757a134b9
Sunghyun Lee <98dltjdgus@naver.com>
2025-10-12 10:23:05 +0900

    5339번-콜센터(브론즈 5, 백준)
    - 구현을 활용한 문제 풀이.
    - 주어진 예제 출력과 동일하게 결과물을 출력한다.

---
+++ exercise/Beakjoon/구현/5339(콜센터)/Problem5339.java
@@ -0,0 +1,30 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Problem5339 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(new Solver().solve());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        public String solve() {
+            return """
+                         /~\\
+                        ( oo|
+                        _\\=/_
+                       /  _  \\
+                      //|/.\\|\\\\
+                     ||  \\ /  ||
+                    ============
+                    |          |
+                    |          |
+                    |          |
+                                        """;
+        }
+    }
+}
