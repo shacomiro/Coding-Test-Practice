d2577288ebac5dcf243fedda910439c3aa8e1e7c
Sunghyun Lee <98dltjdgus@naver.com>
2025-11-09 09:27:35 +0900

    32951번-AI 선도대학(브론즈 5, 백준)
    - 수학, 사칙연산을 활용한 문제 풀이.
    - 주어진 연도 N에서 2024를 뺀 값을 출력한다.

---
+++ exercise/Beakjoon/수학/사칙연산/32951(AI 선도대학)/Problem32951.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem32951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(new Solver(Integer.parseInt(br.readLine())).solve());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        private int year;
+
+        public Solver(int year) {
+            this.year = year;
+        }
+
+        public String solve() {
+            return String.valueOf(year - 2024);
+        }
+    }
+}
