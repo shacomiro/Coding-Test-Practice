0526b1dba0e8a371ad904da7e88d9113ad20ff8f
Sunghyun Lee <98dltjdgus@naver.com>
2025-11-21 17:34:09 +0900

    10179번-쿠폰(브론즈 3, 백준)
    - 수학, 구현, 사칙연산을 활용한 문제 풀이.
    - 주어진 금액에서 20% 할인된 금액을 구해 소수점 둘째 자리까지 출력한다.

---
+++ exercise/Beakjoon/수학/사칙연산/10179(쿠폰)/Problem10179.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            bw.write(new Solver(Double.parseDouble(br.readLine())).solve());
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        private double price;
+        public Solver(double price) {
+            this.price = price;
+        }
+
+        public String solve() {
+            return String.format("$%.2f", price - (price * 0.2));
+        }
+    }
+}
+
