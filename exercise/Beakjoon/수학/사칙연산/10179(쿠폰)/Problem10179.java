769966fdfc51be55c0f270f4b12a2f70f823fbbf
Sunghyun Lee <98dltjdgus@naver.com>
2025-11-22 17:07:04 +0900

    10180번 클래스명 수정
    - Main 대신 문제 번호를 뜻하는 클래스명으로 수정.

--- exercise/Beakjoon/수학/사칙연산/10179(쿠폰)/Problem10179.java
+++ exercise/Beakjoon/수학/사칙연산/10179(쿠폰)/Problem10179.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class Problem10179 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             bw.write(new Solver(Double.parseDouble(br.readLine())).solve());
@@ -20,6 +20,7 @@ public class Main {
 
     static class Solver {
         private double price;
+
         public Solver(double price) {
             this.price = price;
         }
@@ -29,4 +30,3 @@ public class Main {
         }
     }
 }
-
