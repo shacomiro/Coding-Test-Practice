65a41d88dd2d8fafa13a456b10e115d1888377a0
Sunghyun Lee <98dltjdgus@naver.com>
2025-12-09 20:43:09 +0900

    11908번-카드(브론즈 3, 백준)
    - 구현을 활용한 문제 풀이.
    - 카드 두 장을 골라서 숫자가 작은 카드에 적힌 숫자의 누적합을 구할 때, 카드가 1장 남을 때까지 반복해서 얻을 수 있는 최대값을 구해야 한다.
    - 승현이는 두 카드 중 작은 숫자가 적힌 카드를 고르므로, 마지막에 남는 카드 1장은 가장 큰 숫자를 가진 카드이다.
    - 따라서 카드 뭉치의 전체 숫자 합에서 가장 큰 숫자를 뺀 값을 출력한다.

---
+++ exercise/Beakjoon/구현/11908(카드)/Problem11908.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem11908 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        bw.write(new Solver(n, arr).solve());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        private int n;
+        private int[] cards;
+
+        public Solver(int n, int[] cards) {
+            this.n = n;
+            this.cards = cards;
+        }
+
+        public String solve() {
+            return String.valueOf(Arrays.stream(cards).sum() - Arrays.stream(cards).max().getAsInt());
+        }
+    }
+}
