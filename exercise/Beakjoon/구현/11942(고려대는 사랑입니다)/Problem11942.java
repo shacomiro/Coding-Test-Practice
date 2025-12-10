03c9cd431ea53690b1ecfe6f5110a8422850c11d
Sunghyun Lee <98dltjdgus@naver.com>
2025-12-10 20:25:11 +0900

    11942번-고려대는 사랑입니다(브론즈 5, 백준)
    - 구현을 활용한 문제 풀이.
    - "고려대학교"를 출력한다.

---
+++ exercise/Beakjoon/구현/11942(고려대는 사랑입니다)/Problem11942.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem11942 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("고려대학교");
+        bw.flush();
+        bw.close();
+    }
+}
