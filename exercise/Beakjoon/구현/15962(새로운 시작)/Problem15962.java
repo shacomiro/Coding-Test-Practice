7f5cd73ebe7e7eb0382154725dae42009320f7db
Sunghyun Lee <98dltjdgus@naver.com>
2026-01-02 16:59:45 +0900

    15962번-새로운 시작(브론즈 5, 백준)
    - 구현을 활용한 문제 풀이.
    - 파이팅!!을 출력한다.

---
+++ exercise/Beakjoon/구현/15962(새로운 시작)/Problem15962.java
@@ -0,0 +1,13 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class Problem15962 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("파이팅!!");
+        bw.flush();
+        bw.close();
+    }
+}
