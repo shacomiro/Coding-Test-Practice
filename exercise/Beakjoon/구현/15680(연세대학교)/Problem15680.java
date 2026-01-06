1b2bcef11246b816afaed99cd809811041fa32ab
Sunghyun Lee <98dltjdgus@naver.com>
2026-01-06 19:28:55 +0900

    15680번-연세대학교(브론즈 5, 백준)
    - 구현을 활용한 문제 풀이.
    - 입력 N이 0이면 연세대학교 영문명을, 1이면 연세대학교 슬로건을 출력한다.

---
+++ exercise/Beakjoon/구현/15680(연세대학교)/Problem15680.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem15680 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(new Solver(Integer.parseInt(br.readLine())).solve());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        private int n;
+
+        public Solver(int n) {
+            this.n = n;
+        }
+
+        public String solve() {
+            return (n == 0) ? "YONSEI" : "Leading the Way to the Future";
+        }
+    }
+}
