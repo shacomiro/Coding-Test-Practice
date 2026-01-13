25c60ba380b5fd34d40ef454a6a8d20efcee5fe7
Sunghyun Lee <98dltjdgus@naver.com>
2026-01-13 20:13:16 +0900

    17263번-Sort 마스터 배지훈(브론즈 3, 백준)
    - 구현을 활용한 문제 풀이.
    - N개의 정수를 가진 배열 A를 오름차순 정렬했을 때 가장 마지막 원소를 구해야 한다.
    - 오름차순 정렬의 마지막 원소는 최대값이므로, 정렬 없이 배열 A를 순회하여 최대값을 구해 출력한다.

---
+++ exercise/Beakjoon/구현/17263(Sort 마스터 배지훈)/Problem17263.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem17263 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        bw.write(new Solver(n, arr).solve());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        private int n;
+        private int[] a;
+
+        public Solver(int n, int[] a) {
+            this.n = n;
+            this.a = a;
+        }
+
+        public String solve() {
+            return String.valueOf(Arrays.stream(a).max().getAsInt());
+        }
+    }
+}
