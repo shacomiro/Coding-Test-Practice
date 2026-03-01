c40fdc0eca068af78671d6fac7d9c41ab468c82f
Sunghyun Lee <98dltjdgus@naver.com>
2026-03-01 15:13:12 +0900

    29699번-Welcome to SMUPC!(브론즈 5, 백준)
    - 수학, 구현, 문자열, 사칙연산을 활용한 문제 풀이.
    - 라벨지에 적힌 14글자 문구가 반복되는 규칙을 사용하여 N번째 글자가 어느 글자인지 구해 출력한다.

---
+++ exercise/Beakjoon/문자열/29699(Welcome to SMUPC!)/Problem29699.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem29699 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(new Solver(Integer.parseInt(br.readLine())).solve());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        private static final String LABEL = "WelcomeToSMUPC";
+        private int n;
+
+        public Solver(int n) {
+            this.n = n;
+        }
+
+        public String solve() {
+            return String.valueOf(LABEL.charAt((n - 1) % 14));
+        }
+    }
+}
