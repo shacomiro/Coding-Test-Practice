bc08edcc6c53b0d404add5690c3a50c70a28d3ba
Sunghyun Lee <98dltjdgus@naver.com>
2026-04-09 19:32:15 +0900

    32288번-바코드 닉네임(브론즈 4, 백준)
    - 구현, 문자열을 활용한 문제 풀이.
    - 주어진 바코드 닉네임의 I는 i로, l은 L로 변환하여 출력한다.

---
+++ exercise/Beakjoon/문자열/32288(바코드 닉네임)/Problem32288.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem32288 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(new Solver(Integer.parseInt(br.readLine()), br.readLine()).solve());
+        bw.flush();
+        bw.close();
+    }
+
+    static class Solver {
+        private int n;
+        private String nickname;
+
+        public Solver(int n, String nickname) {
+            this.n = n;
+            this.nickname = nickname;
+        }
+
+        public String solve() {
+            return nickname.replaceAll("I", "i").replaceAll("l", "L");
+        }
+    }
+}
